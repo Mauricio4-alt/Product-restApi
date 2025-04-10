7bffd6c8f36763afeb848e3f6be85a40fef8a5f4
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-10 17:56:06 -0500

    verificando respuesta del servidor

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 from products import products
+
+
+@app.route('/ping')
+def ping():
+    return "pong"
+
 if __name__=="__main__":
     app.run(debug=True,port=4000)
