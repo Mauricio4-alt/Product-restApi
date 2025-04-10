af6505f9c1e74e7e8a353d6799c3debfdfc2842e
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-10 18:50:32 -0500

    lista de productos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 from products import products
@@ -6,7 +6,9 @@ from products import products
 
 @app.route('/ping')
 def ping():
-    return "pong"
-
+    return jsonify({"message":"pong"})
+@app.route('/products')
+def getProducts():
+    return jsonify ({"products":products})
 if __name__=="__main__":
     app.run(debug=True,port=4000)
