e44319c712b5aecee3772d872b5a6e066da7b498
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-10 21:48:53 -0500

    agregando insertar datos y agregando actualizar datos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 from products import products
@@ -19,5 +19,25 @@ def getProduct(product_name):
     if (len(productsFound)> 0):
         return jsonify({"Products":productsFound[0]})
     return jsonify ({"message":"Product not found"})
+
+@app.route("/products",methods=["POST"])
+def addProduct():
+    new_product ={
+        "name":request.json['name'],
+        "price":request.json['price'],
+        "quantity":request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({'message':'product Added Succesfull',
+                    'products':products})
+@app.route('/products/<product_name>')
+def editProduct(product_name) :
+    productFound= [product for product in products if product['name'] == product_name]  
+    if (len(productFound)> 0):
+        productFound['name'] = request.json['name']
+        productFound['price'] = request.json['price']
+        productFound['quantity'] = request.json['quantity']
+        return jsonify({'message':'data updated succesfull'})
+    return jsonify({'error':'item not found'})
 if __name__=="__main__":
     app.run(debug=True,port=4000)
