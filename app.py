bc37f65ac57e6df6b7caefa641a93f05f2b0fa67
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-10 19:05:12 -0500

    agregando lista de productos y filtros para lista de productos

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ def ping():
     return jsonify({"message":"pong"})
 @app.route('/products')
 def getProducts():
-    return jsonify ({"products":products})
+    return jsonify ({
+        "message":"Product's list",
+        "products":products
+        })
+@app.route('/products/<name>')
+def getProduct(name):
+    return jsonify ({
+        "message":"Products found",
+        "product":[products[i] for i in range(len(products)) if products[i]["name"] == name ]
+        })
 if __name__=="__main__":
     app.run(debug=True,port=4000)
